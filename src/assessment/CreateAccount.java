2986e896b9e8ed6e3600a6af60db3400fe9104dc
KirraKotsenburg <90931675+KirraKotsenburg@users.noreply.github.com>
2023-04-11 12:36:22 -0600

    Update CreateAccount.java

--- src/assessment/CreateAccount.java
+++ src/assessment/CreateAccount.java
@@ -1,12 +1,14 @@
 package assessment;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class CreateAccount {
 	
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		FirefoxDriver driver = new FirefoxDriver();
 	}
 
 }
